56ca5dc30f411a92cd6be809f4e4f9c61a42abba
halfwintr@gmail.com <halfwintr@gmail.com>
2018-09-20 17:32:48 -0700

    Section 8

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/gatsby-icon.png')} width="30" alt="" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/courses">Downloads</Link>
+          <Link to="/courses">Workshops</Link>
+          <Link to="/courses"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-import Layout from '../components/layout'
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import Card from '../components/Card';
+import Section from '../components/Section';
+import Wave from '../components/Wave';
 
 const IndexPage = () => (
   <Layout>
@@ -10,8 +12,44 @@ const IndexPage = () => (
         <h1>Learn to design and code React apps</h1>
         <p>Learn to implement your design and apply basic styling using powerful CSS techniques. Additionally, you’ll be able to use custom fonts from Google fonts and set up the structure for your images and CSS.</p>
         <Link to="/page-2/">Do the thing</Link>
+        <div className="Logos">
+          <img src={require('../images/logo-sketch.png')} width="50" alt="" />
+          <img src={require('../images/logo-figma.png')} width="50" alt="" />
+          <img src={require('../images/logo-studio.png')} width="50" alt="" />
+          <img src={require('../images/logo-framer.png')} width="50" alt="" />
+          <img src={require('../images/logo-react.png')} width="50" alt="" />
+          <img src={require('../images/logo-swift.png')} width="50" alt="" />
+        </div>
+        <Wave />
       </div>
     </div>
+      <div className="Cards">
+        <h2>11 courses, more coming</h2>
+        <div className="CardGroup">
+          <Card 
+            title="React for Designers"
+            text="12 Sections"
+            image={require('../images/wallpaper.jpg')} />
+          <Card 
+            title="React for Designers"
+            text="12 Sections"
+            image={require('../images/wallpaper4.jpg')} />
+          <Card 
+            title="React for Designers"
+            text="12 Sections"
+            image={require('../images/wallpaper2.jpg')} />
+          <Card 
+            title="React for Designers"
+            text="12 Sections"
+            image={require('../images/wallpaper3.jpg')} />
+      </div>
+    </div>
+    <Section
+      image={require('../images/wallpaper2.jpg')}
+      logo={require('../images/logo-react.png')}
+      title="React for Designers"
+      text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify." 
+    />
   </Layout>
 )
 
